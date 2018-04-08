d73174ffc529cb00d43a34fde986a0ddbdc617fa
Ignacio Iglesias <ignacio.iglesias@hotmail.es>
2018-04-08 13:49:15 +0200

    Added more wallets, address polling and started telegram integration

--- config.lua
+++ config.lua
@@ -5,10 +5,30 @@ local _M = {}
 
 _M.binance = {
     BinanceWallet = "0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be",
+    BinanceWallet_1 = "0xd551234ae421e3bcba99a0da6d736074f22192ff",
+    BinanceWallet_2 = "0x564286362092D8e7936f0549571a803B203aAceD",
 }
 
 _M.bittrex = {
     Bittrex = "0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98",
+    Bittrex_2 = "0xe94b04a0fed112f3664e45adb2b8915693dd5ff3",
+}
+
+_M.bitfinex = {
+    Bitfinex  = "0x7180EB39A6264938FDB3EfFD7341C4727c382153",
+    Bitfinex_1 = "0xcAfB10eE663f465f9d10588AC44eD20eD608C11e",
+    Bitfinex_Wallet1 = "0x1151314c646Ce4E0eFD76d1aF4760aE66a9Fe30F",
+    Bitfinex_Wallet2 = "0x7727E5113D1d161373623e5f49FD568B4F543a9E",
+    Bitfinex_Wallet3 = "0x4fdd5eb2fb260149a3903859043e962ab89d8ed4",
+    Bitfinex_Wallet4 = "0x876EabF441B2EE5B5b0554Fd502a8E0600950cFa",
+}
+
+_M.kraken = {
+	Kraken_1  = "0x2910543Af39abA0Cd09dBb2D50200b3E800A63D2",
+	Kraken_2  = "0x0A869d79a7052C7f1b55a8EbAbbEa3420F0D1E13",
+	Kraken_3  = "0x0a869d79a7052c7f1b55a8ebabbea3420f0d1e13",
+    Kraken_3  = "0xe853c56864a2ebe4576a807d26fdc4a0ada51919",
+    Kraken_4  = "0x267be1c1d684f78cb4f6a176c4911b741e4ffdc0",
 }
 
 return _M
--- tracker.lua
+++ tracker.lua
@@ -1,4 +1,5 @@
 -- tracker.lua https://github.com/iicc1/Exchange-wallets-tracker-bot
+-- Thanks to ethplorer.io for its great api https://github.com/EverexIO/Ethplorer
 
 require('utils')
 local config = require('config')
@@ -9,6 +10,7 @@ local https = require('ssl.https')
 
 local ethplorer_key = "?apiKey=freekey"
 local ethplorer_endpoint = "https://api.ethplorer.io/getAddressInfo/"
+local telegram_token = ""
 
 -- Loading exchange address
 for exchange, wallets in pairs(config) do
@@ -17,66 +19,82 @@ for exchange, wallets in pairs(config) do
 	end
 end
 
--- Checking token balances
-local tokens = 0
-for exchange, wallets in pairs(config) do
-	print("Checking " .. exchange .. " ethereum wallets:")
-	for wallet_name, eth_address in pairs(wallets) do
-		print("For " .. wallet_name .. "(" .. eth_address .. ")")
-		local res = https.request(ethplorer_endpoint .. config[exchange][wallet_name] .. ethplorer_key)
-		local tab = json.decode(res)
-		local balance, name, symbol, price, mk, decimalsc
-		for _, token_tbl in pairs(tab["tokens"]) do
-			for general, info in pairs(token_tbl) do
-				if general == "balance" then balance = info
-				elseif general == "tokenInfo" then
-					for data, value in pairs(info) do
-						if data == "name" then name = value
-						elseif data == "symbol" then symbol = value
-						elseif data == "decimals" then decimals = tonumber(value)
-						elseif data == "price" and type(value) == "table" then
-							for data_price, value_price in pairs(value) do
-								if data_price == "rate" then price = value_price
-								elseif data_price == "marketCapUsd" then mk = value_price end
+-- Polling exchange balances every 5 minutes
+local last_time = os.time()
+while true do
+	if os.time() - last_time > 300 then
+		-- Checking token balances
+		local tokens = 0
+		for exchange, wallets in pairs(config) do
+			print("Checking " .. exchange .. " ethereum wallets:")
+			for wallet_name, eth_address in pairs(wallets) do
+				print(wallet_name .. " (" .. eth_address .. ")")
+				local res = https.request(ethplorer_endpoint .. config[exchange][wallet_name] .. ethplorer_key)
+				local tab = json.decode(res)
+				local balance, name, symbol, price, mk, decimals
+				local token_tab, next = tab["tokens"], next
+				if not token_tab then break end
+				for _, token_tbl in pairs(token_tab) do
+					for general, info in pairs(token_tbl) do
+						if general == "balance" then balance = info
+						elseif general == "tokenInfo" then
+							for data, value in pairs(info) do
+								if data == "name" then name = value
+								elseif data == "symbol" then symbol = value
+								elseif data == "decimals" then decimals = tonumber(value)
+								elseif data == "price" and type(value) == "table" then
+									for data_price, value_price in pairs(value) do
+										if data_price == "rate" then price = value_price
+										elseif data_price == "marketCapUsd" then mk = value_price end
+									end
+								elseif data == "price" and type(value) ~= "table" then
+									price = nil
+									mk = nil
+								end
 							end
-						elseif data == "price" and type(value) ~= "table" then
-							price = nil
-							mk = nil
 						end
 					end
-				end
-			end
-			if decimals > 1 then
-				while decimals > 0 do
-					balance = balance / 10
-					decimals = decimals -1
-				end
-			end
-			if type(price) == "string" then
-				local wallet_value = tonumber(balance) * tonumber(price)
-				-- If there is a significant value of a token in the wallet, then probably listed
-				if wallet_value > 100000 then
-					-- Checks if it was a huge amount of tokens in the previous checks
-					local stored_tokens = redis:smembers(exchange .. ":" .. wallet_name)
-					local search = false
-					for _, tokens_data in pairs(stored_tokens) do
-						if string.find(tokens_data, name) and  string.find(tokens_data, symbol) then
-							search = true
-							redis:srem(exchange .. ":" .. wallet_name, tokens_data)
+					if decimals > 1 then
+						while decimals > 0 do
+							balance = balance / 10
+							decimals = decimals -1
 						end
 					end
-					if search then
-						-- Token was already there, just update data
-						redis:sadd(exchange .. ":" .. wallet_name, symbol .. ":" .. name .. ":" .. balance .. ":" .. price)
-					else
-						-- New big amount of tokens detected in wallet
-						print("A huge amount of tokens " .. name .. " (" .. symbol .. "), has been transfered to this wallet. The total value of this tokens in the wallet is now " .. wallet_value .. "$.")
-						redis:sadd(exchange .. ":" .. wallet_name, symbol .. ":" .. name .. ":" .. balance .. ":" .. price)
+					if type(price) == "string" then
+						local wallet_value = tonumber(balance) * tonumber(price)
+						-- If there is a significant value of a token in the wallet, then probably listed
+						if wallet_value > 100000 then
+						-- Checks if it was a huge amount of tokens in the previous checks
+							local stored_tokens = redis:smembers(exchange)
+							local search = false
+							for _, tokens_data in pairs(stored_tokens) do
+								local symbol_, name_ = string.match(tokens_data, "^(.*):(.*):.*:.*")
+								if symbol_ == symbol and name_ == name then
+									search = true
+									redis:srem(exchange , tokens_data)
+								end
+							end
+							if search then
+								-- Token was already there, just update data
+								redis:sadd(exchange, symbol .. ":" .. name .. ":" .. balance .. ":" .. price)
+							else
+								-- New big amount of tokens detected in wallet
+								-- Only notifies if the exchange was previously saved
+								if redis:exists(exchange) then
+									local text = "[" .. exchange .. " - " .. wallet_name .. "] A huge amount of tokens " .. name .. " (" .. symbol .. "), has been transferred to this wallet. The total value of this tokens in the wallet is now " .. wallet_value .. "$. https://etherscan.io/address/" .. eth_address
+									print(text)
+									-- just a test
+									https.request("https://api.telegram.org/bot" .. telegram_token .. "/sendMessage?chat_id=6722095&text=" .. text)
+									redis:sadd(exchange, symbol .. ":" .. name .. ":" .. balance .. ":" .. price)
+								end
+							end
+						end
 					end
+					tokens =  tokens + 1
 				end
 			end
-			tokens =  tokens + 1
 		end
+		print("Completed: " .. tokens .. " tokens scanned.")
+		last_time = os.time()
 	end
 end
-print("Completed: " .. tokens .. " tokens scanned.")
